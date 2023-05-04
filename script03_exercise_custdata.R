66cd5ff60dfb886ee10c0ae2ab0a6e8933809ba6
Gero Szepannek <gero.szepannek@hochschule-stralsund.de>
2023-05-04 09:50:27 +0200

    exercise + data added

---
+++ script03_exercise_custdata.R
@@ -0,0 +1,22 @@
+## import the following data:
+cdata <- read.table("cdata.tsv", header=T, sep="\t")
+
+############################################################################
+### ~> Analysis target:
+### Predict customers who don't have health insurance (variable health.ins)!
+
+
+## EXERCISES:
+## ----------
+
+# 1) Create a summary of the data...
+# 2) What is the 'prior' (i.e. the probability to have health insurance w/o any additional model)?
+# 3) Missing values: What do you notice?
+# 4) What do you notice w.r.t. ...income?
+# 5)                           ...age?
+# 6)                           ...the occurence of the categories of state.of.res?  
+# 7) What about the variable custid: Should it be used to predict the health insurance status?     
+# 8) Analyze the variable exploratorily: Which of the variables is mostly suited to predict the health insurance status!
+#    Create a 3 minute pitch presentation with your key results!
+
+
